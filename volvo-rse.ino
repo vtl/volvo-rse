613fe812dcd86c23de708ddb7f8187f5e0ba28a4
Vitaly Mayatskikh <vitaly@gravicappa.info>
2018-04-18 09:44:03 -0400

    Use channel A for left monitor, B - for right

--- volvo-rse.ino
+++ volvo-rse.ino
@@ -45,13 +45,14 @@ void setup_canbus()
 
 void setup()
 {
+  Serial.begin(115200);
   serial.println("start");
   setup_canbus();
   pinMode(DISPLAY_ON, INPUT_PULLUP);
   serial.println("done"); 
 }
 
-void send_frame(CANRaw *can, unsigned char *data)
+void send_frame(char *name, CANRaw *can, unsigned char *data)
 {
   CAN_FRAME out;
 
@@ -61,40 +62,51 @@ void send_frame(CANRaw *can, unsigned char *data)
   out.priority = 4;
   out.length = 8;
   memcpy(out.data.byte, data, 8);
-  print_frame("out", &out);
+  print_frame(name, &out);
   can->sendFrame(out);
 }
 
-unsigned char on[2][8]  = {{ 0x03, 0x90, 0x06, 0x01, 0x01, 0x00, 0x00, 0x00 },
-                           { 0x01, 0xb5, 0x07, 0x01, 0x01, 0x00, 0x00, 0x00 }};
+unsigned char on_a[4][9]  = {{ 0x03, 0x90, 0x06, 0x01, 0x01, 0x00, 0x00, 0x00, 4 },
+                             { 0x01, 0xb5, 0x07, 0x01, 0x01, 0x00, 0x00, 0x00, 160 },
+                             { 0x03, 0xA3, 0x08, 0x01, 0x01, 0x00, 0x00, 0x00, 100 },
+                             { 0x03, 0xB8, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0}};
 
-unsigned char off[2][8] = {{ 0x01, 0xb5, 0x08, 0x01, 0x01, 0x00, 0x00, 0x00 },
-                           { 0x03, 0x90, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00 }};
+unsigned char on_b[4][9]  = {{ 0x03, 0x90, 0x06, 0x01, 0x01, 0x00, 0x00, 0x00, 4 },
+                             { 0x01, 0xb5, 0x07, 0x01, 0x01, 0x00, 0x00, 0x00, 160 },
+                             { 0x03, 0xA3, 0x08, 0x01, 0x02, 0x00, 0x00, 0x00, 100 },
+                             { 0x03, 0xB8, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0}};
+
+unsigned char off[2][9] = {{ 0x01, 0xb5, 0x0a, 0x01, 0x01, 0x00, 0x00, 0x00, 1 },
+                           { 0x03, 0x90, 0x0b, 0x01, 0x00, 0x00, 0x00, 0x00, 0 }};
 
 void do_display(bool status)
 {
-  static int seq_id = 0;
-  int delay_ms;
-  unsigned char *p1, *p2;
-  
+  static int seq_id = 1;
+  unsigned char *pa[8], *pb[8];
+  int n;
+
   if (status) {
-    p1 = on[0];
-    p2 = on[1];
-    delay_ms = 333;  
+    n = 4;
+    for (int i = 0; i < n; i++) {
+      pa[i] = on_a[i];
+      pb[i] = on_b[i];
+    }
   } else {
-    p1 = off[0];
-    p2 = off[1];
-    delay_ms = 100;
+    n = 2;
+    for (int i = 0; i < n; i++) {
+      pa[i] = off[i];
+      pb[i] = off[i];
+    }
   }
 
-  p1[2] = seq_id++;
-  p2[2] = seq_id++;
-
-  send_frame(&Can0, p1);
-  send_frame(&Can1, p1);
-  delay(delay_ms);
-  send_frame(&Can0, p2);
-  send_frame(&Can1, p2);  
+  for (int i = 0; i < n; i++) {
+    pa[i][2] = seq_id;
+    pb[i][2] = seq_id;
+    seq_id++;
+    send_frame("A", &Can0, pa[i]);
+    send_frame("B", &Can1, pb[i]);
+    delay(pa[i][8] * 100);
+  }
 }
 
 void loop()
@@ -102,7 +114,7 @@ void loop()
   static bool last_display_status = false;
   bool display_status = !digitalRead(DISPLAY_ON);
 
-//  serial.printf("last_display_status %d, display_status %d\n", last_display_status, display_status);
+  serial.printf("last_display_status %d, display_status %d\n", last_display_status, display_status);
 
   if (last_display_status != display_status) {
     last_display_status = display_status;
